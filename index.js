74ae790ed7d48cff5f717b2def99c4827ec25143
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-05 21:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function produceDrivingRange(range) {
-  return function (range) {
-    range > 10 ? false : true
+  return function (block1, block2) {
+    let distance = Math.abs(parseInt(block1) - parseInt(block2))
+    if(distance < range) {
+      return `within range by ${range - distance}`
+    } else {
+      return `${distance - range} blocks out of range`
+    }
   }
 }
